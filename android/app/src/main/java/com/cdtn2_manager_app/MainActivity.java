7b3e2852bd0e807ab72aed8b3e4ac2aecbbd4683
HuySon <huyson201@gmail.com>
2021-09-15 17:03:35 +0700

    first commit

---
+++ android/app/src/main/java/com/cdtn2_manager_app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cdtn2_manager_app;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "CDTN2_manager_app";
+  }
+}
